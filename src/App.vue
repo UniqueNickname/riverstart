cccc3d6ee4260940ef24cb391882370dbf36444f
UniqueNickname <dr.art-47@yandex.ru>
2022-03-05 15:30:04 +0600

    feat: WIP add input to App

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,28 @@
 <template>
-  <main>
-    main
+  <main class="main">
+    <AInput v-model:value="value" placeholder="Введите имя" />
   </main>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import { ref, watch } from "vue"
+import { makeRequest } from "./api/makeRequest";
+import AInput from "./components/a-input.vue"
 
+const value = ref("")
+
+watch(() => value.value, (current) => makeRequest({
+  url: "https://dadata.ru/api/suggest/name/",
+  method: "POST",
+  data: current,
+}))
 </script>
+
+<style lang="postcss">
+.main {
+  margin: 0 auto;
+  height: 24rem;
+  max-width: 17.5rem;
+  background-color: #C4C4C4;
+}
+</style>
